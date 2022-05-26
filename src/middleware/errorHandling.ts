3d0f84eec52455a3c53a1c00da1a0b903288ed44
Wellington Leardini <wellington-158@hotmail.com>
2022-05-25 21:49:46 -0300

    lint

--- src/middleware/errorHandling.ts
+++ src/middleware/errorHandling.ts
@@ -2,13 +2,13 @@ import { Request, Response, NextFunction } from "express"
 import log from "../utils/loggers"
 
 const errorHandling = (err: Error, req: Request, res: Response, next: NextFunction) => {
-	log.fatal({ message: err.message })
-	if (err instanceof Error) {
-		return res.status(400).json({ message: err.message })
-	}
-	return res.status(500).json({
-		status: "error",
-		message: `Internal server error: ${err}`,
-	})
+    log.fatal({ message: err.message })
+    if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+    }
+    return res.status(500).json({
+        status: "error",
+        message: `Internal server error: ${err}`,
+    })
 }
 export { errorHandling }
