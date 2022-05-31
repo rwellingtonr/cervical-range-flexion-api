90e63235b920791941eae3ad806365b1c1ade49f
Wellington Leardini <wellington-158@hotmail.com>
2022-05-30 22:56:19 -0300

    adicionado método de medição

--- src/modules/measurement/measurement.ts
+++ src/modules/measurement/measurement.ts
@@ -1,14 +1,20 @@
 import { serialPort, parser } from "../../config/serialPort"
-import log from "../../utils/loggers"
 import { io } from "../../app"
 import { Socket } from "socket.io"
+import { PatientHistoryService } from "../patientHistory/patientHistoryServices"
+import log from "../../utils/loggers"
 
 interface IPatientData {
-    name: string
-    score: number[]
+    patientId: string
+    coffito: string
+    score?: number[]
 }
 
-const patientData: IPatientData = { name: "", score: [] }
+const patientData: IPatientData = {
+    patientId: "",
+    coffito: "",
+    score: [0],
+}
 
 serialPort.on("data", (data) => log.debug("Serial data: ", data))
 serialPort.on("readable", () => log.debug("Readable Port: ", serialPort.read()))
@@ -19,11 +25,39 @@ parser.on("readable", () => log.debug("Parser readable: ", parser.read()))
 
 //
 io.on("connection", (socket: Socket) => {
-    socket.on("start-measurement", ({ patientName }) => {
-        patientData.name = patientName
+    socket.on("start", ({ patientId, coffito }: IPatientData) => {
+        // [] enviar sinal ao uC para iniciar a coleta
+        patientData["patientId"] = patientId
+        patientData["coffito"] = coffito
 
         log.debug("start-measurement")
     })
 
-    socket.on("end-measurement", () => log.debug("end"))
+    socket.on("tare", () => {
+        /*
+        [] envia o sinal ao micro controlador para resetar os parâmetros
+        dos sensores
+        */
+        log.debug("Tare")
+    })
+
+    socket.on("stop", async () => {
+        //[]  para o envio de dados do uC
+
+        const { coffito, patientId, score } = patientData
+        log.debug("end")
+
+        const avg = average(score)
+
+        const history = new PatientHistoryService()
+        await history.appendPatientMeasurements(patientId, avg, coffito)
+    })
+    socket.on("abort", () => {
+        //[]  para o envio de dados do uC
+        patientData["score"] = [0]
+    })
+
+    socket.on("close", (reason) => log.debug(reason))
 })
+
+const average = (arr: number[]) => arr.reduce((a, b) => a + b, 0) / arr.length
