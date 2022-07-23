8a1a18acd1eee0a9ba13820a3474ab7cff0aabbf
Wellington Leardini <wellington-158@hotmail.com>
2022-07-23 11:00:20 -0300

    rota no auth

--- src/router/physiotherapistRoute.ts
+++ src/router/physiotherapistRoute.ts
@@ -4,15 +4,18 @@ import { physioController } from "../modules/physiotherapist/physiotherapistModu
 
 const router = Router()
 const physiotherapistRouter = Router()
+const noAuthRoute = Router()
 
 router.get("/", (req, res) => physioController.findAll(req, res))
 
 router
     .route("/:professionalId")
     .get((req, res) => physioController.findOne(req, res))
-    .patch((req, res) => physioController.updatePassword(req, res))
     .delete((req, res) => physioController.deleteProfessional(req, res))
 
-physiotherapistRouter.use("/physiotherapist/", ensureAuthenticated, router)
+noAuthRoute.patch("/:professionalId", (req, res) => physioController.updatePassword(req, res))
+
+physiotherapistRouter.use("/physiotherapist", ensureAuthenticated, router)
+physiotherapistRouter.use("/physiotherapist", noAuthRoute)
 
 export default physiotherapistRouter
