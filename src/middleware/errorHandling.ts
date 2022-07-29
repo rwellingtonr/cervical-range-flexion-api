5d1a566f570d991764c82967f4be1c1731cfa94a
Wellington Leardini <wellington-158@hotmail.com>
2022-07-28 21:44:24 -0300

    simplificação

--- src/middleware/errorHandling.ts
+++ src/middleware/errorHandling.ts
@@ -2,10 +2,9 @@ import { Request, Response, NextFunction } from "express"
 import log from "../utils/loggers"
 
 const errorHandling = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    log.fatal({ message: err.message })
-    if (err instanceof Error) {
-        return next(err)
-    }
+    log.fatal(err.message)
+    if (err instanceof Error) return next(err)
+
     return res.status(500).json({
         status: "error",
         message: `Internal server error: ${err}`,
