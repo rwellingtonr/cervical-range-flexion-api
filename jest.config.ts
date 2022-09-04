eef26a3b4b96b6172a1f6b61670fc2a22124c6e1
Wellington Leardini <wellington-158@hotmail.com>
2022-09-04 14:18:47 -0300

    alterada configs do jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,15 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
+import type { Config } from "jest"
 
-export default {
+const config: Config = {
     clearMocks: true,
     coverageProvider: "v8",
     testMatch: ["**/**/*.spec.ts"],
     preset: "ts-jest",
+    displayName: "Cervical Spine Range Flex",
+    maxConcurrency: 20,
 }
+
+export default config
