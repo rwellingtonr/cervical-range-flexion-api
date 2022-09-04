7a009f0ddd7480ad947fe7f95f27ba5edbbec7ed
Wellington Leardini <wellington-158@hotmail.com>
2022-09-04 14:19:31 -0300

    graceful shutdown aprimorado

--- src/middleware/gracefulShutDown.ts
+++ src/middleware/gracefulShutDown.ts
@@ -1,12 +1,15 @@
 import { io, serverHttp } from "../app"
+import { serialPort } from "../config/serialPort"
 import { prisma } from "../database"
 import log from "../utils/loggers"
 
 const gracefulShutDown = (event: string) => {
     return serverHttp.close(() => {
         log.warn(`Ending ${process.pid} with event ${event}`)
+        serialPort.close()
         io.close()
         prisma.$disconnect().catch((e) => log.error(e))
+        log.warn("All process has been closed!")
         process.exit(0)
     })
 }
