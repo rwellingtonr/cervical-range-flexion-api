3d17a170882ca250818893d9aab41a7434da39c1
Wellington Leardini <wellington-158@hotmail.com>
2022-10-19 19:34:03 -0300

    feat: adicionado disconnect do serial port

--- src/middleware/gracefulShutDown.ts
+++ src/middleware/gracefulShutDown.ts
@@ -1,12 +1,15 @@
 import { io, serverHttp } from "@app/index"
 import { prisma } from "@database/index"
+import { errorCb } from "@utils/errorCb"
+import arduinoSerialPort from "@modules/integration/serialPort"
 import log from "@utils/loggers"
 
 const gracefulShutDown = (event: string) => {
     return serverHttp.close(() => {
         log.warn(`Ending ${process.pid} with event ${event}`)
         io.close()
-        prisma.$disconnect().catch((e) => log.error(e))
+        arduinoSerialPort.disconnect().catch(errorCb)
+        prisma.$disconnect().catch(errorCb)
         log.warn("All process has been closed!")
         process.exit(0)
     })
